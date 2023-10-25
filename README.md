646f956d58167dc562730746b5a7ea06ff617f78
mudassarrahman <74159388+mudassarrahman@users.noreply.github.com>
2023-10-25 15:53:04 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+For starting the server
+
+npm run dev
+
+The server runs on port 3005
+
+requirements
+mongodb local server connection
+redis server connection required
+
+Performance:
+For performance the schema based pre and post hooks are used
---
+++ server.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+console.log(process.env.DATABASE);
+const DB = 'mongodb://0.0.0.0:27017';
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('DB connection successful!'))
+  .catch((err) => console.error('Error connecting to the database:', err));
+
+const port = process.env.PORT || 3005;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
